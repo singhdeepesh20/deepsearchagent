e1eeec253a809540de1138aedf153dc1f66f45f4
Deepesh singh <mr.deepesh.singh.20@gmail.com>
2026-04-10 23:03:28 +0530

    update: optimize code

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ arxiv=ArxivQueryRun(api_wrapper=arxiv_wrapper)
 api_wrapper=WikipediaAPIWrapper(top_k_results=1,doc_content_chars_max=200)
 wiki=WikipediaQueryRun(api_wrapper=api_wrapper)
 
-search=DuckDuckGoSearchRun(name="Search")
+search=DuckDuckGoSearchRun(name="Search"
 
 
 st.title("DeepSearch-Your Intelligent Agent ")
